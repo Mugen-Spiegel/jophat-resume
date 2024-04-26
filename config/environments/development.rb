45431fe972cc08006930f204e55282a339418b61
Jophat Tamayo <tamay06131993@gmail.com>
2024-04-26 13:07:05 +0800

    remove ngrok url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,4 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.hosts << "bc4c-120-28-135-242.ngrok-free.app"
 end
